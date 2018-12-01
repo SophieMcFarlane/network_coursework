f04266c41f51ebeffe5030fbb2957d8afb012208
Sophie McFarlane <sophie.e.mcfarlane@durham.ac.uk>
2018-12-01 16:55:22 +0000

    corrected a spelling mistake

--- Server.java
+++ Server.java
@@ -38,7 +38,7 @@ public class Server extends Thread {
    public static void main(String [] args) {
       int port = Integer.parseInt(args[0]);
       try {
-         Thread t = new GreetingServer(port);
+         Thread t = new Server(port);
          t.start();
       } catch (IOException e) {
          e.printStackTrace();
