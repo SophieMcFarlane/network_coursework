fd304bcbaa1784dd0068ec39c33bdabef75d61cc
Sophie McFarlane <sophie.e.mcfarlane@durham.ac.uk>
2018-12-02 14:21:01 +0000

    modifed

--- Server.java
+++ Server.java
@@ -73,7 +73,7 @@ public class Server extends Thread {
          for(int i=0; i<lines.size(); i++){
            String temp = lines.get(i);
            String[] splitString = temp.split("\\s+\\s");
-           for(int j=0; j<splitString.size(); j++){
+           for(int j=0; j<splitString.length()-1; j++){
              System.out.println(splitString.get(i));
            }
          }
