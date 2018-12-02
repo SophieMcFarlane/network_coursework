800dc3fa91b11470cb2675b79901914d2b5fccb1
Sophie McFarlane <sophie.e.mcfarlane@durham.ac.uk>
2018-12-02 14:23:18 +0000

    typo

--- Server.java
+++ Server.java
@@ -54,7 +54,7 @@ public class Server extends Thread {
          BufferedReader bufferedReader = new BufferedReader(fileReader);
          List<String> lines = new ArrayList<String>();
          List<String> titles = new ArrayList<String>();
-         List<String> splitString = new ArrayList<String();
+         List<String> splitString = new ArrayList<String>();
          String line = null;
 
          while((line = bufferedReader.readLine()) != null){
