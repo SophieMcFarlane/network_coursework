cccede5dff0c5dae79fd72ef770e3a04eabc3de5
Sophie McFarlane <sophie.e.mcfarlane@durham.ac.uk>
2018-12-02 14:22:40 +0000

    modifed

--- Server.java
+++ Server.java
@@ -54,6 +54,7 @@ public class Server extends Thread {
          BufferedReader bufferedReader = new BufferedReader(fileReader);
          List<String> lines = new ArrayList<String>();
          List<String> titles = new ArrayList<String>();
+         List<String> splitString = new ArrayList<String();
          String line = null;
 
          while((line = bufferedReader.readLine()) != null){
@@ -72,7 +73,7 @@ public class Server extends Thread {
 
          for(int i=0; i<lines.size(); i++){
            String temp = lines.get(i);
-           String[] splitString = temp.split("\\s+\\s");
+           splitString = temp.split("\\s+\\s");
            for(int j=0; j<splitString.length()-1; j++){
              System.out.println(splitString.get(i));
            }
